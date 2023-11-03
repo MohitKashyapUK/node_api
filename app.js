4863905afdae9656f1672fe5b7d400f692c7be2a
mohit <mohitkashyap46707@gmail.com>
2023-11-03 16:48:15 +0530

    sk

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
+const app = express();
+const port = 3000;
+
 const user_modules_path = "./user_modules/";
 
 const index = require(user_modules_path + "index.js");
-
-const app = express();
-const port = 3000;
 
 // URL root
 app.get('/:name', index);
