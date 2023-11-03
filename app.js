3cc2f0e383c5ca37d5a1576348c74bdb84660e3e
mohit <mohitkashyap46707@gmail.com>
2023-11-03 21:09:22 +0530

    sk

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const port = 3000;
 
 const user_modules_path = "./user_modules/";
 
-const index = require(user_modules_path + "index.js");
+const index = require(user_modules_path + "index");
+const lucky_numbers = require(user_modules_path + "lucky_numbers");
 
 // URL root
+app.get("/", (req, res) => res.send("Hello, this is the home page!"));
+
+app.get("/sk/lucky_numbers", lucky_numbers);
+
 app.get('/sk/:name', index);
 
 // starting express
