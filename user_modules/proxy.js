8ef35dc8ab508a8a2b917f6de0060a2419a230ba
mohit <mohitkashyap46707@gmail.com>
2023-11-13 22:53:40 +0530

    url

--- user_modules/proxy.js
+++ user_modules/proxy.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 function proxy(req, res) {
   const client_headers = req.headers;
 
-  const url = decodeURIComponent(req.query.url);
+  const url = "http%3A%2F%2Fip270035912.ahcdn.com%2Fkey%3Dixo6U0Hps%2B8zPMH0oFX2Pw%2Cend%3D1699897550%2Fstate%3DZVJSak26%2Fbuffer%3D4000000%3A22486092%2C1856.7%2Fspeed%3D207616%2Freftag%3D55309829%2Fssd3%2F65%2F6%2F218699196%2Fb%2F328000%2F328611%2F328611.mp4"; //decodeURIComponent(req.query.url);
 
   const url_object = new URL(url);
 
