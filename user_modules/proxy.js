19bbf0789963f97413e1567c1f30397860eb27ed
mohit <mohitkashyap46707@gmail.com>
2023-11-14 17:56:57 +0530

    proxy

--- user_modules/proxy.js
+++ user_modules/proxy.js
@@ -3,28 +3,40 @@ const axios = require("axios");
 function proxy(req, res) {
   const client_headers = req.headers;
 
-  const range = client_headers.range;
+  const { range, cookie, accept } = client_headers;
 
-  const url = "http://ip270035912.ahcdn.com/key=ixo6U0Hps+8zPMH0oFX2Pw,end=1699897550/state=ZVJSak26/buffer=4000000:22486092,1856.7/speed=207616/reftag=55309829/ssd3/65/6/218699196/b/328000/328611/328611.mp4"; //decodeURIComponent(req.query.url);
+  const url = decodeURIComponent(req.query.url);
 
-  // const url_object = new URL(url);
+  const url_object = new URL(url);
 
   // client_headers.host = url_object.hostname;
+  const hostname = url_object.hostname;
   // client_headers.accept = "*/*";
 
-  const configs = { responseType: "stream" };
+  const configs = { responseType: "stream", headers: { host: hostname } };
+
+  const headers = configs.headers;
 
   if (range) {
-    configs["headers"] = {};
-    configs["headers"]["range"] = range;
+    headers.range = range;
   }
+
+  if (cookie) {
+    headers["cookie"] = cookie;
+  }
+
+  if (accept) {
+    headers["accept"] = accept;
+  }
+
+  configs.headers = headers;
 
   axios.get(url, configs)
     .then((response) => {
       // URL headers
-      const url_headers = JSON.parse(JSON.stringify(response.headers));
+      const url_headers = response.headers; // JSON.parse(JSON.stringify(response.headers));
 
-      // client headers
+      // setting client headers
       res.set(url_headers);
 
       // Read data from the stream.
