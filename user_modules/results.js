42a505824fe873b642f680183549431dbc0f5e33
Mohit <mohitkashyap46707@gmail.com>
2024-04-08 19:32:02 +0530

    jj

--- user_modules/results.js
+++ user_modules/results.js
@@ -1,11 +1,15 @@
-async function main(req, res) {
+function main(req, res) {
     const cheerio = require("cheerio");
-    
-    const $ = await cheerio.fromURL("https://sattakingrecords.com");
-    
-    const element = $("body > div.container-fluid > div > font > div:nth-child(1) > div.game_result > div > font:nth-child(3) > b");
-    
-    res.send(element.toString());
+    const axios = require("axios");
+
+    axios.get()
+    .then(({data}) => {
+        const $ = cheerio.loadBuffer(data);
+        
+        const element = $("body > div.container-fluid > div > font > div:nth-child(1) > div.game_result > div > font:nth-child(3) > b");
+        
+        res.send(element.toString());
+    });
 }
 
 module.exports = main;
