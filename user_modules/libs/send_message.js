5df414e42dd75b9383c8ec9072cfcc5fed55b152
Mohit <mohitkashyap46707@gmail.com>
2024-04-13 20:45:12 +0530

    ff

--- user_modules/libs/send_message.js
+++ user_modules/libs/send_message.js
@@ -4,9 +4,9 @@ const send_text = require("./send_text_to_whatsapp");
 async function main(contents) {
   const sms_api = "https://platform.clickatell.com/messages/http/send";
   const params = {
-  apiKey: "PXTRN_iRQia1LPTtFDzq6g==",
-  to: 918534992433,
-  content: contents
+    apiKey: "PXTRN_iRQia1LPTtFDzq6g==",
+    to: 918534992433,
+    content: contents
   };
 
   // await axios.get(sms_api, { params });
