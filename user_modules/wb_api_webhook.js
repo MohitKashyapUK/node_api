54cc5526bf95933935503a840874759debad1873
Mohit <mohitkashyap46707@gmail.com>
2024-04-14 00:29:05 +0530

    ff

--- user_modules/wb_api_webhook.js
+++ user_modules/wb_api_webhook.js
@@ -1,9 +1,18 @@
 const send_text_to_whatsapp = require("./libs/send_text_to_whatsapp");
 
 async function main(req, res) {
-  const contents = req.body;
-  await send_text_to_whatsapp(contents.toString());
-  res.end();
+  /* const message_object = req.body["messages"][0];
+
+  if (message_object == undefined || !message_object["author"].startsWith("917037127740")) {
+    res.end();
+    process.exit();
+  }
+
+  const text = message_object.body; */
+
+  // await send_text_to_whatsapp(contents.toString());
+
+  res.json(req.body);
 }
 
 module.exports = main;
