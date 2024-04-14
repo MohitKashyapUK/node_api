ab9baecf61e6d13e50a953a0446b4b6372a7fa3a
Mohit <mohitkashyap46707@gmail.com>
2024-04-14 23:55:16 +0530

    fds

--- user_modules/wb_api_webhook.js
+++ user_modules/wb_api_webhook.js
@@ -1,4 +1,5 @@
 const send_text_to_whatsapp = require("./libs/send_text_to_whatsapp");
+const sk_results = require("./libs/sk_results");
 
 async function main(req, res) {
   const message_object = req.body["messages"][0];
@@ -8,7 +9,10 @@ async function main(req, res) {
     process.exit();
   }
 
-  const text = message_object.body;
+  const text = message_object.body.trim().toLowerCase();
+  // const sk_names_array = ["faridabad", "gaziabad", "gali", "disawer", "all"];
+  // const name = sk_names_array.find(value => value == text);
+  // const results = await sk_results();
 
   await send_text_to_whatsapp(text);
 
