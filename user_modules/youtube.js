acddb49c2ad26a0124f02282aa4abf2c6fc8b0c9
Mohit <mohitkashyap46707@gmail.com>
2024-04-24 03:41:38 +0530

    fgsdgdf

--- user_modules/youtube.js
+++ user_modules/youtube.js
@@ -2,17 +2,27 @@
 const ytdl = require("ytdl-core");
 
 function youtube(req, res) {
-  // const url = req.params[0];
+  let url = req.query.url;
+  
+  if (url == "") {
+    res.json({ "error": "YouTube video URL is required." });
+    return;
+  }
 
-  // ytdl.getInfo(url)
-  //   .then(data => {
-  //     const formats = data.formats;
-  //     const videowithaudio = formats.filter(value => value.hasAudio && value.hasVideo);
-  //     res.json(videowithaudio);
-  //   }).catch(error => {
-  //     res.json({ "error": new String(error) });
-  //   })
-  res.json(req.query);
+  url = decodeURIComponent(url);
+
+  try {
+    ytdl.getInfo(url)
+      .then(data => {
+        const formats = data.formats;
+        const videowithaudio = formats.filter(value => value.hasAudio && value.hasVideo);
+        res.json(videowithaudio);
+      }).catch(error => {
+        res.json({ "error": new String(error) });
+      })
+  } catch (error) {
+    res.json({ "error": new String(error) });
+  }
 }
 
 module.exports = youtube;
