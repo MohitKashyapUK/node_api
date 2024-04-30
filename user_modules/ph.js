0771bdf79a2415a9485bb84c728a5e4ddb6df76f
Mohit <mohitkashyap46707@gmail.com>
2024-04-30 16:36:47 +0530

    adfd

--- user_modules/ph.js
+++ user_modules/ph.js
@@ -1,7 +1,7 @@
 const { PornHub } = require('pornhub.js');
-const pornhub = new PornHub();
 
 async function main(req, res) {
+  const pornhub = new PornHub();
   const url = "https://www.pornhub.com/view_video.php?viewkey=652f2087b71a2"; //'https://www.pornhub.com/view_video.php?viewkey=ph5ac81eabe203d';
   const video = await pornhub.video(url);
   res.json(video);
