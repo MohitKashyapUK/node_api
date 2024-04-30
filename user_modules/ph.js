91b7e21900f0fe30acb437d35a9914504005b072
Mohit <mohitkashyap46707@gmail.com>
2024-04-30 16:54:35 +0530

    dfsa

--- user_modules/ph.js
+++ user_modules/ph.js
@@ -1,10 +1,19 @@
 const { PornHub } = require('pornhub.js');
 
 async function main(req, res) {
-  const pornhub = new PornHub();
-  const url = "https://www.pornhub.com/view_video.php?viewkey=652f2087b71a2"; //'https://www.pornhub.com/view_video.php?viewkey=ph5ac81eabe203d';
-  const video = await pornhub.video(url);
-  res.json(video);
+  const parameter = req.query.url;
+  if (!parameter) {
+    res.json({error: "URL is required!"});
+    return;
+  }
+  try {
+    const pornhub = new PornHub();
+    const url = decodeURIComponent(parameter); //"https://www.pornhub.com/view_video.php?viewkey=652f2087b71a2"; //'https://www.pornhub.com/view_video.php?viewkey=ph5ac81eabe203d';
+    const video = await pornhub.video(url);
+    res.json(video);
+  } catch (error) {
+    res.json({error});
+  }
 }
 
 module.exports = main;
