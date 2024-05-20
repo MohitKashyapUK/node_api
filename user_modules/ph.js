bc6d38f9d3d93b22b33b54f7cb0fb429774a9894
Mohit <mohitkashyap46707@gmail.com>
2024-05-20 16:37:39 +0530

    fsdsa

--- user_modules/ph.js
+++ user_modules/ph.js
@@ -1,14 +1,16 @@
 const { PornHub } = require('pornhub.js');
 
 async function main(req, res) {
-  const parameter = req.query.url;
-  if (!parameter) {
-    res.json({error: "URL is required!"});
+  const { id } = req.query;
+
+  if (!id) {
+    res.json({error: "ID is required!"});
     return;
   }
+
   try {
     const pornhub = new PornHub();
-    const url = decodeURIComponent(parameter); //"https://www.pornhub.com/view_video.php?viewkey=652f2087b71a2"; //'https://www.pornhub.com/view_video.php?viewkey=ph5ac81eabe203d';
+    const url = "https://www.pornhub.com/view_video.php?viewkey=" + id; //"https://www.pornhub.com/view_video.php?viewkey=652f2087b71a2"; //'https://www.pornhub.com/view_video.php?viewkey=ph5ac81eabe203d';
     const video = await pornhub.video(url);
     res.json(video);
   } catch (error) {
--- ytdl.js
+++
@@ -1,9 +0,0 @@
-const ytdl = require("ytdl-core");
-
-const id = "miVF5Yn8cOE";
-
-ytdl.getInfo(id)
-.then(data => {
-  // const formats = ytdl.filterFormats(data.formats, "videoonly");
-  console.log(data);
-})
