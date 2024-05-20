b7663850109e4511aca200c9e42e47a2434e3ba9
Mohit <mohitkashyap46707@gmail.com>
2024-05-20 16:55:54 +0530

    dfsad

--- user_modules/ph.js
+++ user_modules/ph.js
@@ -8,14 +8,14 @@ async function main(req, res) {
     return;
   }
 
-  try {
+  // try {
     const pornhub = new PornHub();
     const url = "https://www.pornhub.com/view_video.php?viewkey=" + id; //"https://www.pornhub.com/view_video.php?viewkey=652f2087b71a2"; //'https://www.pornhub.com/view_video.php?viewkey=ph5ac81eabe203d';
     const video = await pornhub.video(url);
     res.json(video);
-  } catch (error) {
-    res.json({error});
-  }
+  // } catch (error) {
+  //   res.json({error});
+  // }
 }
 
 module.exports = main;
