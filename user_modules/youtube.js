2090abaea1a52996c35feb685c0edc6bfab58d0e
Mohit <mohitkashyap46707@gmail.com>
2024-06-11 17:42:31 +0530

    asfsdf

--- user_modules/youtube.js
+++ user_modules/youtube.js
@@ -3,6 +3,7 @@ const ytdl = require("ytdl-core");
 function parse(stream, props) {
   const obj = {};
   props.forEach(key => obj[key] = stream[key]);
+  obj["size"] = formatSize(obj["contentLength"]);
   return obj;
 }
 
@@ -30,6 +31,29 @@ function filter_progressive_streams(formats, props) {
   return array;
 }
 
+function formatSize(bytes) {
+  const baseUnit = 1024;
+  const modernUnit = 1000;
+  let kb = bytes / baseUnit;
+  let size, unit;
+
+  if (kb > modernUnit) {
+    let mb = kb / baseUnit;
+    if (mb > modernUnit) {
+      size = mb / baseUnit;
+      unit = "GB";
+    } else {
+      size = mb;
+      unit = "MB";
+    }
+  } else {
+    size = kb;
+    unit = "KB";
+  }
+
+  return `${size.toFixed(3)}${unit}`;
+}
+
 async function youtube(req, res) {
   try {
     const {id, filter} = req.query; // query parameters
@@ -37,10 +61,10 @@ async function youtube(req, res) {
     if (!id) { // id of yt video
       res.json({error: "ID required."});
       return;
-    } else if (!filter) { // type of resource e.g. video or audio
+    }/* else if (!filter) { // type of resource e.g. video or audio
       res.json({error: "Filter required."});
       return;
-    }
+    } */
   
     const details = await ytdl.getInfo(id); // getting details of video
     const formats = details.formats; // formats or resolution of video
