782d00dfafb919019d8e246bab19d2d1215e0ed0
Mohit <mohitkashyap46707@gmail.com>
2024-06-11 18:15:07 +0530

    fdsf

--- app.js
+++ app.js
@@ -9,43 +9,33 @@ const app = express();
 const port = 3000;
 
 // Middlewares
-app.use(cors());
+app.use(cors()); // cors(cross origin): allowed
 app.use(express.json()); // to parse application/json
 app.use(express.urlencoded({ extended: true })); // to parse application/x-www-form-urlencoded
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the 'public' directory
 
 // Custom libraries
 const user_modules_path = "./user_modules/"; // Base path to the libraries
 
-const sk = require(user_modules_path + "sk");
-const proxy = require(user_modules_path + "proxy");
-// const youtube = require(user_modules_path + "youtube");
-const results = require(user_modules_path + "results");
-const testing = require(user_modules_path + "testing");
-const lucky_numbers = require(user_modules_path + "lucky_numbers");
-
 // URL root
 app.get("/", (req, res) => res.send("Homepage!"));
 
-app.get("/testing", testing);
+app.get("/testing", require(user_modules_path + "testing"));
 
 // get 10 lucky numbers
-app.get("/sk/lucky-numbers", lucky_numbers);
+app.get("/sk/lucky-numbers", require(user_modules_path + "lucky_numbers"));
 
 // get sk results by name
-app.get('/sk/:name', sk);
+app.get('/sk/:name', require(user_modules_path + "sk"));
 
 // '/proxy/<path>' including '/'
-app.get(/^\/proxy\/(.+)/, proxy);
+app.get(/^\/proxy\/(.+)/, require(user_modules_path + "proxy"));
 
 // /^\/youtube\/(.+)/
 app.get("/youtube", require(user_modules_path + "youtube"));
 
 // Get SK results
-app.get(/^\/results?/, results);
+app.get(/^\/results?/, require(user_modules_path + "results"));
 
 // starting express (app)
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
