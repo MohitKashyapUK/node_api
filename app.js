d9258a6f1a6fedd4fa37e2f3a9e268b5cf10471c
Mohit <mohitkashyap46707@gmail.com>
2024-06-19 03:21:57 +0530

    g

--- app.js
+++ app.js
@@ -17,25 +17,12 @@ app.use(express.static(path.join(__dirname, 'public'))); // Serve static files f
 // Custom libraries
 const user_modules_path = "./user_modules/"; // Base path to the libraries
 
-// URL root
-app.get("/", (req, res) => res.send("Homepage!"));
+app.get("/", (req, res) => res.send("Homepage!")); // Main page
+app.get("/testing", require(user_modules_path + "testing")); // Development
+app.get("/sk/lucky-numbers", require(user_modules_path + "lucky_numbers")); // get 10 lucky numbers
+app.get('/sk/:name', require(user_modules_path + "sk")); // get sk results by name
+app.get(/^\/proxy\/(.+)/, require(user_modules_path + "proxy")); // '/proxy/<path>' including '/'
+app.get("/youtube", require(user_modules_path + "youtube")); // /^\/youtube\/(.+)/
+app.get(/^\/results?/, require(user_modules_path + "results")); // Get SK results
 
-app.get("/testing", require(user_modules_path + "testing"));
-
-// get 10 lucky numbers
-app.get("/sk/lucky-numbers", require(user_modules_path + "lucky_numbers"));
-
-// get sk results by name
-app.get('/sk/:name', require(user_modules_path + "sk"));
-
-// '/proxy/<path>' including '/'
-app.get(/^\/proxy\/(.+)/, require(user_modules_path + "proxy"));
-
-// /^\/youtube\/(.+)/
-app.get("/youtube", require(user_modules_path + "youtube"));
-
-// Get SK results
-app.get(/^\/results?/, require(user_modules_path + "results"));
-
-// starting express (app)
-app.listen(port, () => console.log(`Example app listening on port ${port}`));
+app.listen(port, () => console.log(`Example app listening on port ${port}`)); // starting express (app)
